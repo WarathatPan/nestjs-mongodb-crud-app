776468dbb10431fcc07e4709d982f61b5d6de916
Warathat_panthong <warathat.pan@gmail.com>
2021-01-28 13:58:39 +0700

    add module books and crud with mongodb

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjs_db'),
+    BooksModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
